1e3a6e562521848948268ea651875a2356f90cb3
Victor Shulgin <shulgins1978@gmail.com>
2026-05-18 18:56:11 +0800

    edit Rational.cpp

--- homework 2 semestr/Rational/Rational/Rational.cpp
+++ homework 2 semestr/Rational/Rational/Rational.cpp
@@ -90,23 +90,3 @@ std::ostream& operator <<(std::ostream& out, const Rational& r) {
     out << r.numer << "/" << r.denom; 
     return out; 
 }
-
-using namespace std;
-
-int main() {
-	setlocale(LC_ALL, "Russian"); 
-    Rational a(1, 2), b(-1, 6);
-    cout << "a = " << a << ", b = " << b << endl;
-
-    cout << "a + b = " << a + b << endl;
-
-    double val = (double)a;
-    cout << "a as double = " << val << endl;
-
-    Rational g;
-    cout << "Enter rational (numer denom): ";
-    cin >> g;
-    cout << "You entered: " << g << endl;
-
-    return 0;
-}
